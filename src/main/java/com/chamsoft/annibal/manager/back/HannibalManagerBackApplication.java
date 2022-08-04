9fb4c0cb27f4a5ada60924c65eb18c8fb23315d5
toujanichamseddine <toujani.chams@gmail.com>
2022-08-04 14:31:39 +0200

    First commit project creation

---
+++ src/main/java/com/chamsoft/annibal/manager/back/HannibalManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.chamsoft.annibal.manager.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannibalManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannibalManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamsoft/annibal/manager/back/HannibalManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamsoft.annibal.manager.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HannibalManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
